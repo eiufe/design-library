b2a40cb4aa3ad753b6a693e37c671626cd116e2b
Shourya Ranka <shouryaranka@economist.com>
2019-07-27 15:55:37 +0530

    Added first version of dropdown

--- design-library/src/dl.js
+++ design-library/src/dl.js
@@ -1,4 +1,12 @@
 var $ = require("jquery");
 window.$ = $;
+import easydropdown from 'easydropdown';
 import buttonUI from './components/foundation elements/button/button';
 buttonUI();
+
+$(document).ready(function() {
+    $('.eiu-dropdown').on('DOMSubtreeModified', function(e) {
+        $(e.target).addClass("ready");
+    });
+    easydropdown.all();
+});
